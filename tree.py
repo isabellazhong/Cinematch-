dbd36d923611721c8c2738c063c54bc7a7ec0b48
mayyadow <mayadowman2024@gmail.com>
2025-03-26 16:59:06 -0400

    Update tree.py

--- tree.py
+++ tree.py
@@ -1,26 +1,39 @@
 import csv
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import tree
+from MovieData import MovieData
 
 class Movie:
 
-    """The class for the movie, which will be a node in the 
+    """The class for the movie, which will be a node in the
         decision tree when choosing a movie to watch for the user
 
         Instance Attributes:
-            - genre: the genre of the novie 
+            - title: the title of the movie
+            - genre: the genre of the novie
             - duration: how long the movie is in minutes
-            - rating: the rotton tomatoes the movie has 
+            - rating: the rotton tomatoes the movie has
     """
 
+    title: str
     genre: str
     duration: float
-    rating: int 
+    rating: int
 
 
-class MovieTree:
-    
-    """This class is a decision tree to filter out 
-        movies for the user to watch 
+class MovieDecisionTree:
+
+    """This class is a decision tree to filter out
+        movies for the user to watch
     """
+    def __init__(self):
+        self.tree = None
 
-    _root: Movie
-    _subtrees: list[Movie]
+
+    def build_decision_tree(self):
+        movies_dict = MovieData.load_movie_basics()
+
+        features = []
+        predicted_movies = []
